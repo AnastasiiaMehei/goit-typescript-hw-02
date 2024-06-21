3e0922273710e384b9ceebc8aa397686ec224ebc
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-06-21 07:30:53 +0300

    add

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,11 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
+import { Image } from "./components/types";
+
 axios.defaults.baseURL = "https://api.unsplash.com/search";
-export const getImages = async (topic, currentPage) => {
+export const getImages = async (
+  topic: string,
+  currentPage: number
+): Promise<Image[]> => {
   const response = await axios.get("/photos", {
     params: {
       client_id: "-7i_jnQlSjDuNkJ4shZWckNEJtBVks9schHspWR86Vg",
